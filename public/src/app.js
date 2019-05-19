f0865b9871d1077cac75ae11bf88c091762a7b5c
Andre Monello <atmonello@gmail.com>
2019-05-19 01:05:21 -0300

    Implementada lista de participantes

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,12 @@
-import './style/main.scss';
+import "./style/main.scss";
+import fazenda from "../fazenda.json";
+
+const debugTemplate = require("./template/partials/person.handlebars");
+
+function createPeopleList(data) {
+    console.log(data)
+    var debugContainer = document.getElementsByClassName("ranking-list")[0];
+    debugContainer.innerHTML = debugTemplate(data);
+}
+
+createPeopleList(fazenda);
